a7228a3352b60bbee228aa3ea1d84164fd69a191
jameswmccarty <jameswmccarty@gmail.com>
2024-12-16 17:02:21 -0500

    Add files via upload

--- day16.py
+++ day16.py
@@ -28,6 +28,34 @@ def bfs(start, end, walls):
 			heapq.heappush(q,(score+1000,pos,(facing-1)%4))
 	return "None"
 
+def bfs_tiles(start, end, walls, cap):
+	pt_costs = dict()
+	q = []
+	heapq.heapify(q)
+	dirs = [(1,0),(0,-1),(-1,0),(0,1)]
+	heapq.heappush(q, (0,start,0, {start}))
+	found_routes = set()
+	while q:
+		score, pos, facing, route = heapq.heappop(q)
+		if pos == end:
+			if score <= cap:
+				for pt in route:
+					found_routes.add(pt)
+		if score > cap:
+			return found_routes
+		if pos not in pt_costs:
+			pt_costs[pos] = score + 1000
+		if score <= pt_costs[pos]:
+			x, y = pos
+			dx, dy = dirs[facing]
+			nx, ny = x+dx, y+dy
+			if (nx, ny) not in walls and (nx, ny) not in route:
+				heapq.heappush(q,(score+1,(nx,ny),facing, {*route, (nx,ny)}))
+		if score+1000 <= pt_costs[pos]:
+			heapq.heappush(q,(score+1000,pos,(facing+1)%4, route))
+			heapq.heappush(q,(score+1000,pos,(facing-1)%4, route))
+	return "None"
+
 if __name__ == "__main__":
 
 	# Part 1 Solution
@@ -42,8 +70,9 @@ if __name__ == "__main__":
 				elif char == 'S': start = (x,y)
 				elif char == 'E': end = (x,y)
 			y = y + 1
-	print(bfs(start, end, walls))
-
+	cost = bfs(start, end, walls)
+	print(cost)
 
 	# Part 2 Solution
-
+	print(len(bfs_tiles(start, end, walls, cost)))
+		
