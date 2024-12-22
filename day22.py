04fd60bcda33ce4930db0513284bb90aad6800b4
jameswmccarty <jameswmccarty@gmail.com>
2024-12-22 01:22:35 -0500

    Add files via upload

--- day22.py
+++ day22.py
@@ -2,6 +2,11 @@
 
 # Advent of Code 2024 Day 22
 
+matrix = []
+deltas = []
+
+delta_avail_amount = dict()
+
 def rng(seed):
 	while True:
 		seed = ((seed * 64) ^ seed) % 16777216
@@ -16,11 +21,29 @@ if __name__ == "__main__":
 	with open("day22_input", "r") as infile:
 		for line in infile:
 			a = rng(int(line.strip()))
+			m = [ int(line.strip()) % 10 ]
 			for _ in range(2000):
 				b = next(a)
+				m.append(b%10)
 			total += b
+			matrix.append(m)
 	print(total)
 
+
 	# Part 2 Solution
-
-
+	for line in matrix:
+		d = [ line[i] - line[i-1] for i in range(1,len(line)) ]
+		deltas.append(d)
+	y = 0
+	for row in deltas:
+		seen = set()
+		for i in range(len(row)-3):
+			delta_run = tuple(row[i:i+4])
+			if delta_run not in seen:
+				seen.add(delta_run)
+				banannas = matrix[y][i+4]
+				if delta_run not in delta_avail_amount:
+					delta_avail_amount[delta_run] = 0
+				delta_avail_amount[delta_run] += banannas
+		y += 1
+	print(max(v for v in delta_avail_amount.values()))
